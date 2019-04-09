804b79ef460793e36e4400f7327ce4aaab3e0021
Azirrarth <azirrarth@gmail.com>
2019-04-09 15:41:47 -0300

    Delegate Action com Foreach e Lambda Expression

---
+++ Expressões lambda, delegates, LINQ/DelegateActioncomFORECH/DelegateActioncomFORECH/Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace DelegateActioncomFORECH.Entities
+{
+    class Product
+    {
+        public string  Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Expressões lambda, delegates, LINQ/DelegateActioncomFORECH/DelegateActioncomFORECH/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using DelegateActioncomFORECH.Entities;
+
+
+namespace DelegateActioncomFORECH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            Action<Product> act = p => { p.Price += p.Price * 0.1;};
+
+            list.ForEach(p => { p.Price += p.Price * 0.1; });
+
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+        static void UpdatePrice(Product p)
+        {
+            p.Price += p.Price * 0.1;
+        }
+    }
+}
