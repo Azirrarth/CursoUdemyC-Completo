8fd3115372f56a807142a51b590c572fe10ebedd
Azirrarth <azirrarth@gmail.com>
2019-04-09 09:23:42 -0300

    Finalizado Topico Especial 2 Extention Methods

---
+++ Topocos Especiais Parte2/ExtentionMethods1/ExtentionMethods1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace System
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2019, 4, 9, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+            string s1 = "God morning dear students!";
+            Console.WriteLine(s1.Cut(10));
+            
+        }
+    }
+}
