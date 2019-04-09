34c7f142f9cd91ec61ed98ac0b6113458cfb473e
Azirrarth <azirrarth@gmail.com>
2019-04-09 10:47:25 -0300

    Introdução ao Delegate

---
+++ Expressões lambda, delegates, LINQ/DelegatesExe/DelegatesExe/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using DelegatesExe.Services;
+
+namespace DelegatesExe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Sum;
+            BinaryNumericOperation op2 = CalculationService.Max;
+           
+
+            double result = op(a, b);
+            double result2 = op2(a, b);
+            Console.WriteLine(result);
+            Console.WriteLine(result2);
+        
+        }
+    }
+}
---
+++ Expressões lambda, delegates, LINQ/DelegatesExe/DelegatesExe/Services/CalculationService.cs
@@ -0,0 +1,20 @@
+﻿namespace DelegatesExe.Services
+{
+    delegate double BinaryNumericOperation(double n1, double n2);
+
+    class CalculationService
+    {
+        public static double Max(double x, double y)
+        {
+            return (x > y) ? x : y;
+        }
+        public static double Sum(double x, double y)
+        {
+            return x + y;
+        }
+        public static double Square(double x)
+        {
+            return x * x;
+        }
+    }
+}
