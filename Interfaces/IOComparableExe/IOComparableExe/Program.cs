40540cf396ad05984c770f8f13f731a8f0f974d0
Azirrarth <azirrarth@gmail.com>
2019-04-09 08:36:26 -0300

    Final do capitulo Interfaces IComparable

---
+++ Interfaces/IOComparableExe/IOComparableExe/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using IOComparableExe.Entities;
+
+namespace IOComparableExe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\Curso Udemy C#\Interfaces\temp\in.txt";
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach (Employee emp in list)
+                    {
+                        Console.WriteLine(emp);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+    }
+}
