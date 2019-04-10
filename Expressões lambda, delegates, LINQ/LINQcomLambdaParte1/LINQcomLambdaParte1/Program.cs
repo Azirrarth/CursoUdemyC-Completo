49baab455740833cde33bf4b2b804cd5f1945ee5
Azirrarth <azirrarth@gmail.com>
2019-04-10 09:25:11 -0300

    LINQ com Lambda Expression Where e Select e anonymous object

--- Expressões lambda, delegates, LINQ/LINQcomLambdaParte1/LINQcomLambdaParte1/Program.cs
+++ Expressões lambda, delegates, LINQ/LINQcomLambdaParte1/LINQcomLambdaParte1/Program.cs
@@ -42,6 +42,9 @@ namespace LINQcomLambdaParte1
 
             var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
             Print("NAMES OF PRODUCTS FROM TOOLS:", r2);
+
+            var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new {p.Name, p.Price, CategoryName = p.Category.Name});
+            Print("NAMES STARTED WITH C AND ANONYMOUS OBJECT:", r3);
         }
     }
 }
