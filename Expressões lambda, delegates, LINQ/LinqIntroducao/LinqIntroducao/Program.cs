56c4bca02aae4ac44d2cfcb9be64ae92c7028571
Azirrarth <azirrarth@gmail.com>
2019-04-10 07:57:46 -0300

    Introdução ao LINQ com Lambda Expression

---
+++ Expressões lambda, delegates, LINQ/LinqIntroducao/LinqIntroducao/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace LinqIntroducao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // especivicar o datasource
+
+            int[] numbers = new int[] { 2, 3, 4, 5};
+
+            // definir a expressão query
+
+            IEnumerable<int> result = numbers.Where(x => x % 2 == 0).Select(x => x * 10);
+
+            // Executar a query
+
+            foreach (int x in result)
+            {
+                Console.WriteLine(x);
+            }
+
+
+        }
+    }
+}
