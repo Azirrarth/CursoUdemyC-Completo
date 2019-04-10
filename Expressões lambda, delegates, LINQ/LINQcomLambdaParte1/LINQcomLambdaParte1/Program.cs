88709eee19febabddbcdc897be2eee14b55f6050
Azirrarth <azirrarth@gmail.com>
2019-04-10 09:33:26 -0300

    LINQ com Lambda Expression Where e Select e anonymous object Spike e Take

--- Expressões lambda, delegates, LINQ/LINQcomLambdaParte1/LINQcomLambdaParte1/Program.cs
+++ Expressões lambda, delegates, LINQ/LINQcomLambdaParte1/LINQcomLambdaParte1/Program.cs
@@ -45,6 +45,12 @@ namespace LINQcomLambdaParte1
 
             var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new {p.Name, p.Price, CategoryName = p.Category.Name});
             Print("NAMES STARTED WITH C AND ANONYMOUS OBJECT:", r3);
+
+            var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+            Print("TIER 1 ORDER BY PRICE THEN BY NAME:", r4);
+
+            var r5 = r4.Skip(2).Take(4);
+            Print("TIER 1 ORDER BY PRICE THEN BY NAME SKIP 2 TAKE 4", r5);
         }
     }
 }
